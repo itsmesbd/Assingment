1f9f36a951233b676b4084a28c57ea68d686702a
Shivbijaydeep <shivbijaydeep@outlook.com>
2021-09-23 11:11:21 +0530

    completed simple calculator

---
+++ JavaScript Assingment3/Calculator.js
@@ -0,0 +1,27 @@
+let lightTheme = "Calculator.css";
+let darkTheme = "dark.css";
+
+// Clears the screen on click of C button.
+function clearScreen() {
+  document.getElementById("result").value = "";
+}
+// Displays entered value on screen.
+function Show(value) {
+  let res = document.getElementById("result");
+  if(res.value == "undefined"){
+    res.value = "";
+  }
+  res.value += value;
+}
+// Swaps the style sheet in order to  achieve dark mode.
+function changeTheme() {
+  let darkMode = document.getElementById("dark-mode");
+  let theme = document.getElementById("theme");
+  if (theme.getAttribute("href") == lightTheme) {
+    theme.href = darkTheme;
+    darkMode.innerHTML = "Light Mode";
+  } else {
+    theme.href = lightTheme;
+    darkMode.innerHTML = "Dark Mode";
+  }
+}
