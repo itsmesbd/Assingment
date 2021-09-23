56a684c6114b7b3c5c1fe60103f6ee492ca0dbde
Shivbijaydeep <shivbijaydeep@outlook.com>
2021-09-23 18:11:20 +0530

    done with the calculator

--- JavaScript Assingment3/Calculator.js
+++ JavaScript Assingment3/Calculator.js
@@ -3,7 +3,7 @@ let darkTheme = "dark.css";
 
 // Clears the screen on click of C button.
 function clearScreen() {
-  document.getElementById("result").value = "";
+  document.getElementById("result").value = ""; 
 }
 // Displays entered value on screen.
 function Show(value) {
